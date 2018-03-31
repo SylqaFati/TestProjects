cbeb80d1d9154a8cbf43601b0389ac822d2ecf9b
SylqaFati <insidemanphatt@gmail.com>
2018-03-31 02:12:47 +0200

    word counter

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int numberofCharacters = Console.ReadLine().ToCharArray().Count();
+
+            //string word = "this task is for testing only";
+            Console.WriteLine("enter your words");
+
+            string word = Console.ReadLine();
+
+
+
+
+
+            int numberOfCharacters = word.ToCharArray().Count();
+
+         
+
+            //foreach (var item in word)
+            //{
+            //    numberOfCharacters++;
+
+            //}
+
+            //int averageWpm = (130 / 60)*60;
+            
+          
+
+
+            Console.ReadLine();
+            Console.WriteLine("there are {0} words in this sentence", numberOfCharacters);
+            int tmp = numberOfCharacters;
+            Console.ReadLine();
+
+
+
+          
+
+
+            int wpm = (tmp / 60) * 60
+                ;
+            Console.WriteLine("your text duration is : {0}  minutes", wpm);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
---
+++ ConsoleApp1/WordCounter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your words here : ");
+            string  text = Console.ReadLine();
+            int wordCount = 0, index = 0;
+            while(index<text.Length)
+            {
+
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+                wordCount++;
+
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("there are {0} words", wordCount);
+            Console.ReadLine();
+
+            int wpm = (wordCount / 60) * 60;
+
+            //Console.WriteLine("text duration is {0} minutes", wpm);
+            //Console.ReadLine();
+        }
+    }
+}
